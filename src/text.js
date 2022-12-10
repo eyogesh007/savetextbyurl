fbc642241ffb8b5086aa19d59590888c543937a9
eyogesh007 <81839475+eyogesh007@users.noreply.github.com>
2022-12-10 19:04:23 +0530

    Add files via upload

--- src/text.js
+++ src/text.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import {useState} from 'react'
+import {useState,useEffect} from 'react'
 import { useParams } from 'react-router-dom';
 import {useNavigate} from 'react-router';
 
@@ -11,14 +11,17 @@ import {useNavigate} from 'react-router';
         {savedtext:''}
         )
         const {savedtext}=text
-        if(savedtext==='')
-        axios.get(`https://write-1ad81-default-rtdb.firebaseio.com/${name}.json`).then(res=>{setText({...text,savedtext:res.data.savedtext});});  
+        useEffect(()=>{
+          axios.get(`https://write-1ad81-default-rtdb.firebaseio.com/${name}.json`).then(res=>{setText({...text,savedtext:res.data.savedtext});});  
+      }, []) 
+       
     
 
   const [url,setUrl] = useState(name)
 
   function submittext(){
     axios.put(`https://write-1ad81-default-rtdb.firebaseio.com/${url}.json`,text);
+    alert('saved successfuully')
 }
 
 function back(){
@@ -31,7 +34,7 @@ return (
         <label>url:</label>
       <input value={url} style ={{marginTop:'20px',marginLeft:'10px',padding:'3px'}} type='text' placeholder='enter url' onChange={e=>setUrl(e.target.value)} ></input>
       <br></br>
-      <textarea value={savedtext} style={{margin:'20px',height:'70vh',width:'90%'}} onChange={e=>{setText({...text,savedtext:e.target.value})}} placeholder="enter text" height="90" width="90"></textarea>
+      <textarea value={savedtext} style={{margin:'20px',height:'70vh',width:'90%'}} onChange={e=>{setText({savedtext:e.target.value})}} placeholder="enter text" height="90" width="90"></textarea>
       <br></br>
       <input type="button" style={{margin:'20px'}} onClick={submittext} value="SUBMIT"></input>
       <input type="button" style={{margin:'20px'}} onClick={back} value="Try other url"></input>
